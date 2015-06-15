fb30785ebc2e70ab70def4947fa4bbb924c09d78
Jim Pelton <jimpelton@u.boisestate.edu>
2015-06-15 14:58:51 -0600

    added block and blockcollection classes to volume folder

---
+++ include/bd/volume/block.h
@@ -0,0 +1,75 @@
+#ifndef block_h__
+#define block_h__
+
+#include <bd/graphics/texture.h>
+
+#include <bd/scene/transformable.h>
+
+#include <glm/fwd.hpp>
+
+#include <string>
+#include <vector>
+
+#include <algorithm>
+
+namespace bd {
+
+class ShaderProgram;
+
+
+//////////////////////////////////////////////////////////////////////////
+/// \brief Associates volumetric data with a location in world coordinates.
+/// A blocks transform can be used to position a set of proxy geometry in
+/// 3D world space. The block's texture contains the GL id/name of the 3D
+/// texture that should be sampled by the proxy geometry.
+//////////////////////////////////////////////////////////////////////////
+class Block : public bd::Transformable
+{
+public:
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    /// \brief Create block (i,j,k) with specified dims and world origin.
+    /// \param[in] ijk The block ID (and position with in the grid of blocks).
+    /// \param[in] dims The dimensions in data points of this block.
+    /// \param[in] origin Origin of this block in world coords (or whatever coords you want...maybe...sigh...whatevs).
+    ///////////////////////////////////////////////////////////////////////////////
+    Block(const glm::u64vec3 &ijk, const glm::vec3 &dims, const glm::vec3 &origin);
+
+    virtual ~Block();
+
+
+    /// \brief Set/get the ijk location of this block.
+    glm::u64vec3 ijk() const;
+    void ijk(const glm::u64vec3 &ijk);
+
+
+    /// \brief Set/get if this block is marked empty.
+    void empty(bool);
+    bool empty() const;
+
+
+    /// \brief Set/get the average value of this here block.
+    void avg(float);
+    float avg() const;
+    
+
+    /// \brief Get the texture assoc'd with this block.
+    Texture& texture();
+
+
+    /// \brief String rep. of this blockeroo.
+    virtual std::string to_string() const override;
+
+private:
+    glm::u64vec3 m_ijk;  ///< Block's location in block coordinates.
+    bool m_empty;        ///< True if this block was determined empty.
+    float m_avg;         ///< Avg. val. of this block.
+    Texture m_tex;       ///< Texture data assoc'd with this block.
+};
+
+std::ostream& operator<<(std::ostream &os, const Block &b);
+
+} // namespace bd
+
+#endif // !block_h__
---
+++ src/bd/volume/block.cpp
@@ -0,0 +1,117 @@
+
+
+#include <bd/volume/block.h>
+
+#include <bd/log/gl_log.h>
+#include <bd/util/util.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <sstream>
+
+
+namespace bd {
+
+///////////////////////////////////////////////////////////////////////////////
+//      Static   Members
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//     Instance  Methods
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+Block::Block(const glm::u64vec3 &ijk, const glm::vec3 &dims, const glm::vec3 &origin)
+    : m_ijk{ ijk }
+    , m_empty{ false }
+    , m_avg{ 0.0f }
+    , m_tex{  }
+{
+    transform().scale(dims);
+    transform().origin(origin);
+    update();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+Block::~Block()
+{
+}
+
+//void Block::draw()
+//{
+//
+//}
+
+///////////////////////////////////////////////////////////////////////////////
+glm::u64vec3 Block::ijk() const
+{
+    return m_ijk;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+void Block::ijk(const glm::u64vec3 &ijk)
+{
+    m_ijk = ijk;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+bool Block::empty() const
+{
+    return m_empty;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+void Block::empty(bool b)
+{
+    m_empty = b;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+float Block::avg() const
+{
+    return m_avg;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+void Block::avg(float a)
+{
+    m_avg = a;
+}
+
+Texture& Block::texture()
+{
+    return m_tex;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+std::string Block::to_string() const
+{
+    std::stringstream ss;
+    ss <<  "{ ijk: (" << m_ijk.x << ',' << m_ijk.y << ',' << m_ijk.z << ")\n"
+        "Origin: " << m_transform.origin().x << ',' << m_transform.origin().y << ',' <<
+        m_transform.origin().z <<
+        "Empty: " << (m_empty ? "True\n" : "False\n") << "\n"
+        "Texture: " << m_tex << " }";
+
+    return ss.str();
+}
+
+std::ostream& operator<<(std::ostream &os, const Block &b)
+{
+    return os << b.to_string();
+}
+
+} // namespace bd
