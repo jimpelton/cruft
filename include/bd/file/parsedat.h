e30ebacdf611c556f2b1001977c725cc490c48da
Jim Pelton <jimpelton@u.boisestate.edu>
2015-08-09 20:06:44 -0600

    added dat file parsing and started test folder

---
+++ include/bd/file/parsedat.h
@@ -0,0 +1,39 @@
+#ifndef parsedat_h__
+#define parsedat_h__
+
+#include <bd/file/datatypes.h>
+
+#include <string>
+#include <fstream>
+
+namespace bd
+{
+
+
+struct DatFileData
+{
+    DatFileData() : rX{ 0 }, rY{ 0 }, rZ{ 0 }, volumeFileName{ "" },
+        dataType{ bd::DataType::Float }
+    { }
+
+    // resolution
+    unsigned int rX;
+    unsigned int rY;
+    unsigned int rZ;
+
+    // file name of raw file
+    std::string volumeFileName;
+
+    // data type of raw file
+    bd::DataType dataType;
+};
+
+std::ostream& operator<<(std::ostream &, const DatFileData&);
+
+bool
+parseDat(const std::string &datfile, DatFileData &data);
+
+} // namespace
+
+
+#endif  // !parsedat_h__
---
+++ test/test_parsedat/main.cpp
@@ -0,0 +1,28 @@
+
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <bd/file/parsedat.h>
+#include <bd/file/datatypes.h>
+
+//#include <iostream>
+
+#define RES_FOLDER "./res"
+
+TEST_CASE("parseDat parses", "[file][parsedat]")
+{
+    const std::string fileName{ RES_FOLDER"/walnut.dat" };
+
+    bd::DatFileData dfd;
+    bool result = bd::parseDat(fileName, dfd);
+
+    REQUIRE( result == true );
+    REQUIRE( dfd.volumeFileName == "walnut.raw" );
+    REQUIRE( dfd.dataType == bd::DataType::UnsignedShort);
+    REQUIRE( dfd.rX == 400 );
+    REQUIRE( dfd.rY == 296 );
+    REQUIRE( dfd.rZ == 352 );
+}
+
+
