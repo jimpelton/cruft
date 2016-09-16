5e373e082164b340fe6513d509521e139a6ee3bd
Jim Pelton <jimpelton@u.boisestate.edu>
2016-09-16 14:53:06 -0600

    some octree stuff

--- include/bd/datastructure/octree.h
+++ include/bd/datastructure/octree.h
@@ -5,16 +5,27 @@
 #ifndef bd_octree_h
 #define bd_octree_h
 
+#include <glm/glm.hpp>
 #include <array>
 
 namespace bd
 {
 
-template<typename Ty>
+    /// \brief A very simple Octree implementation.
+    /// Template Parameters:
+    ///   VecTy -  The type of 3D vector to use for min, max corner points.
+    ///            For example, I want to use  an int type vector for 
+    ///            because my octree will subdivide an array, and I want to use
+    ///            min max points for array indexes.
+    ///   DataTy - The data type stored in this tree.
+template<typename VecTy, typename DataTy>
 class Octree
 {
 public:
-  Octree()
+  Octree(VecTy const &origin, VecTy const &halfDim)
+    : m_origin{ origin }
+    , m_halfDim{ halfDim }
+    , m_data{ nullptr }
   {
   }
 
@@ -24,8 +35,15 @@ public:
   }
 
 
+  void
+
+
 private:
-  std::array<Octree<Ty>*, 8> m_children;
+  std::array<Octree<VecTy, DataTy>*, 8> m_children;
+  VecTy m_origin;
+  VecTy m_halfDim;
+
+  DataTy *m_data;
 
 }; // class Octree
 
