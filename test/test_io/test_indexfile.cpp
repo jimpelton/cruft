fadf33107781074db57d40fea41d5fa4d756090c
Jim Pelton <jimpelton@u.boisestate.edu>
2017-02-26 14:01:59 -0700

    test_io folder

---
+++ test/test_io/test_indexfile.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jim on 2/11/17.
+//
+
+
+#include <bd/io/fileblock.h>
+#include <bd/io/indexfile.h>
+
+#include <catch.hpp>
+
+
+TEST_CASE("I don't know yet")
+{
+  bd::IndexFile index_file;
+  index_file.init(bd::DataType::Character);
+
+}
---
+++ test/test_io/test_io_main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Jim Pelton on 9/1/16.
+//
+
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
